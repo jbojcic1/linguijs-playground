ecb47a597265fbbc089c0bc1dcaffaa0592cf968
Josip Bojcic <jbojcic1@gmail.com>
2019-08-19 16:16:33 +0200

    Add lingui.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { I18nProvider } from '@lingui/react';
+import enCatalog from './locales/en/messages';
+import hrCatalog from './locales/hr/messages';
+import { setupI18n } from '@lingui/core';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const language = 'hr';
+const catalogs = {
+    en: enCatalog,
+    hr: hrCatalog
+};
+
+export const i18n = setupI18n({ language, catalogs });
+
+ReactDOM.render(
+    <I18nProvider i18n={i18n} language={language}>
+        <App />
+    </I18nProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
